ec73f70ddd016278ad3fe18bb8543ef4dd2b0764
Artem Drushchyts <temafm-3@yandex.ru>
2020-06-20 19:59:29 +0300

    feat: add start window

---
+++ src/components/Games/SpeakIt/Game/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './index.scss';
+import {
+  Container, Col, Row,
+} from 'react-bootstrap';
+
+function Game() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>SpeakIt </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Game;
--- src/components/Games/SpeakIt/index.tsx
+++ src/components/Games/SpeakIt/index.tsx
@@ -1,8 +1,40 @@
 import * as React from 'react';
+import {
+  Container, Col, Row, Button,
+} from 'react-bootstrap';
+import './index.scss';
+import { useState } from 'react';
+import Game from './Game';
 
 function SpeakIt() {
+  const [game, setGame] = useState(false);
+
   return (
-    <div />
+    <div>
+      { game
+        ? (<Game />)
+        : (
+          <Container className="text-center intro" fluid>
+            <Row style={{ position: 'relative', top: '30%' }}>
+              <Col>
+                <h2 className="title">SPEAKIT</h2>
+                <p className="intro-text">
+                  Click on the words to hear them sound.
+                  <br />
+                  Click on the button and speak the words into the microphone.
+                </p>
+                <Button
+                  variant="dark"
+                  className="intro-btn"
+                  onClick={() => setGame(true)}
+                >
+                  Start
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        )}
+    </div>
   );
 }
 
