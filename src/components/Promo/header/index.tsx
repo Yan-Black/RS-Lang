0860e93d7bf80d20efd33af118ee299418d7c57e
zzh0c <zzh0czz@gmail.com>
2020-07-05 15:04:00 +0300

    feat: add header

--- src/components/Promo/header/index.tsx
+++ src/components/Promo/header/index.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import './index.scss';
 
 const Header: React.FC = () => (
-  <div />
+  <div className="promo-header-container">
+    <p>RS LANGUAGE</p>
+  </div>
 );
 
 export default Header;
