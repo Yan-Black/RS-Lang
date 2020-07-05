da4efd3952b533cc7eddb02e754d851bdbdfee11
zzh0c <zzh0czz@gmail.com>
2020-07-05 15:10:32 +0300

    refactor: change devCards design

--- src/components/Promo/body/aboutUs/cardContainer/card/index.tsx
+++ src/components/Promo/body/aboutUs/cardContainer/card/index.tsx
@@ -7,7 +7,7 @@ const DevCard: React.FC = () => (
   <Card className="dev-card">
     <Card.Img variant="top" src="https://pic.mysku-st.ru/uploads/pictures/06/81/37/2018/07/22/b76793.jpg" />
     <Card.Body>
-      <Card.Title>Card Title</Card.Title>
+      <Card.Title>KONO DIO DA!</Card.Title>
       <Card.Text>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -16,7 +16,7 @@ const DevCard: React.FC = () => (
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </Card.Text>
-      <Button variant="outline-primary" href="https://github.com/Yan-Black/RS-Lang">Git</Button>
+      <Button variant="light" href="https://github.com/Yan-Black/RS-Lang">Git</Button>
     </Card.Body>
   </Card>
 );
