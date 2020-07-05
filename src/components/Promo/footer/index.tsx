c4fdfb0a4d54ff33f3f013f48796558ddc9aff35
zzh0c <zzh0czz@gmail.com>
2020-07-05 15:49:35 +0300

    feat: add footer

--- src/components/Promo/footer/index.tsx
+++ src/components/Promo/footer/index.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import './index.scss';
 
 const Footer: React.FC = () => (
-  <div />
+  <footer className="promo-footer">
+    <p className="promo-footer-text">Developed 2020</p>
+    <a href="https://github.com/Yan-Black/RS-Lang" className="promo-footer-link">LINK</a>
+  </footer>
 );
 
 export default Footer;
