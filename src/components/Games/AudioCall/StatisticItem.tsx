66e17d38e45952e7d1db0e5aaf74b4c80d8bc516
Stepanenko Oksana <lactivo4ka@gmail.com>
2020-07-08 00:05:33 +0300

    feat: show trash icon near the word only if users dictionary includes this word

--- src/components/Games/AudioCall/StatisticItem.tsx
+++ src/components/Games/AudioCall/StatisticItem.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import { Json } from 'containers/Games/AudioCall/models';
+import { useSelector, useDispatch } from 'react-redux';
+import { State } from 'models';
 
 function StatisticItem(currWord: {item: Json}): JSX.Element {
   if (!currWord) {
     return null;
   }
+
+  const dispatch = useDispatch();
+  const learningWords: Array<Json> = useSelector(
+    (state: State) => state.dictionaryState.learningWords,
+  );
+  const difficultWords: Array<Json> = useSelector(
+    (state: State) => state.dictionaryState.difficultWords,
+  );
+  const trashIconClass = (learningWords.includes(currWord.item) || difficultWords.includes(currWord.item)) ? 'fas fa-trash ml-auto mr-3' : 'd-none';
 
   async function playWordAudio() {
     const audioUrl: string = currWord.item.audio;
@@ -26,7 +37,7 @@ function StatisticItem(currWord: {item: Json}): JSX.Element {
   }
 
   const speakerIconClickHandler = () => playWordAudio();
-  const speakerIconKeyPressHandler = (
+  const keyPressHandler = (
     event: React.KeyboardEvent<HTMLElement>,
   ) => event.preventDefault();
   const trashMouseOverHandler = (
@@ -36,10 +47,15 @@ function StatisticItem(currWord: {item: Json}): JSX.Element {
     event: React.MouseEvent<HTMLElement, MouseEvent>,
   ) => unHovered(event);
   const trashFocusHandler = (event: React.FocusEvent<HTMLElement>) => hovered(event);
+  const trashClickHandler = () => {
+    learningWords.includes(currWord.item);
+    // if (learningWords.includes(currWord.item)) dispatch()
+    // ; difficultWords.includes(currWord.item);
+  };
 
   return (
     <div className="d-flex bg-light my-2 align-items-baseline">
-      <i className="fas fa-volume-down mr-3" role="button" aria-label="Speaker icon" tabIndex={-1} style={{ cursor: 'pointer' }} onClick={speakerIconClickHandler} onKeyPress={speakerIconKeyPressHandler} />
+      <i className="fas fa-volume-down mr-3" role="button" aria-label="Speaker icon" tabIndex={-1} style={{ cursor: 'pointer' }} onClick={speakerIconClickHandler} onKeyPress={keyPressHandler} />
       <span className="bg-light d-inline-block text-truncate" style={{ width: '70%' }}>
         <span className="text-primary">{currWord.item.word}</span>
         {' '}
@@ -47,7 +63,7 @@ function StatisticItem(currWord: {item: Json}): JSX.Element {
         {' '}
         {currWord.item.wordTranslate}
       </span>
-      <i className="fas fa-trash ml-auto mr-3" style={{ cursor: 'pointer' }} data-toggle="tooltip" data-placement="left" title="Удалить из словаря" onMouseOver={trashMouseOverHandler} onMouseLeave={trashMouseLeaveHandler} onFocus={trashFocusHandler} />
+      <i className={trashIconClass} style={{ cursor: 'pointer' }} data-toggle="tooltip" data-placement="left" title="Удалить из словаря" tabIndex={-1} role="button" aria-label="Trash icon" onMouseOver={trashMouseOverHandler} onMouseLeave={trashMouseLeaveHandler} onFocus={trashFocusHandler} onClick={trashClickHandler} onKeyPress={keyPressHandler} />
     </div>
   );
 }
