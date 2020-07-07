479f8c337ad1511f60d49723c2eb99ecafd9cbd1
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-07-07 15:32:31 +0300

    fix: eslint errors

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import Authorization from 'components/Authorization';
 import Main from 'components/Main';
 import './index.scss';
 
 function App() {
   return (
     <div className="app">
-      {/* <Authorization /> */}
-        <Main />
+      <Main />
     </div>
   );
 }
