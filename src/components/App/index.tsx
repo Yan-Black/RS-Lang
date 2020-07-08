6a2481dc96f7c3d2911e59cc3a891592dff8550a
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-07-08 22:02:54 +0300

    fix: resolved problems

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
+import Authorization from 'components/Authorization';
 import Main from 'components/Main';
 import './index.scss';
 
-function App() {
-  return (
-    <div className="app">
-      <Main />
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div>
+    <Authorization />
+    <Main />
+  </div>
+);
 
 export default App;
