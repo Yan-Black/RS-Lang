3042f6cf03acb9f65ea0c70d54b5ba1a789b283b
Artem Drushchyts <temafm-3@yandex.ru>
2020-07-12 11:15:05 +0300

    refactor: remove duplicate lines

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -31,10 +31,6 @@ import loaderReducer from '../containers/Games/EnglishPuzzle/GameBlock/GameBoard
 import activeIndexReducer from '../containers/Games/EnglishPuzzle/GameController/activeIndexReducer';
 import helpBtnsReducer from '../containers/Games/EnglishPuzzle/GameBlock/GameBoard/HelpButtons/reducer';
 import collectionReducer from '../containers/Games/EnglishPuzzle/GameBlock/GameBoard/collectionReducer';
-import levelControlReducer from '../containers/Games/SpeakIt/ControlLevel/reducer';
-import fetchReducer from '../containers/Games/SpeakIt/FetchGroup/reducer';
-import cardsReducer from '../containers/Games/SpeakIt/CardsGroup/reducer';
-import startGameReducer from '../containers/Games/SpeakIt/ControlButtons/reducer';
 
 const rootReducer = combineReducers({
   app: appReducer,
