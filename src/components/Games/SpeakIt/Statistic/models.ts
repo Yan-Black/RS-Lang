e321c8e11576e0e1092e46b39a9938d5000ef6e4
Artem Drushchyts <temafm-3@yandex.ru>
2020-07-12 11:19:43 +0300

    refactor: delete unused code

--- src/components/Games/SpeakIt/Statistic/models.ts
+++
@@ -1,5 +0,0 @@
-// export interface StatisticProps {
-//   wordsToApply: Card[];
-//   setCheckedStateToCards: React.Dispatch<React.SetStateAction<string[]>>;
-//   setDragging: React.Dispatch<React.SetStateAction<boolean>>;
-// }
